b108119299e63710f36d2384740fba232f80b083
Marc Fabre <nds.kram@gmail.com>
2023-06-14 15:49:37 +0200

    Corrected init

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -35,8 +35,9 @@ export default {
         });
 
         let lastCodeTimestamp = 0;
+        let starting = false;
 
-        return { codeValue, setCodeValue, camerasValue, setCamerasValue, lastCodeTimestamp };
+        return { codeValue, setCodeValue, camerasValue, setCamerasValue, lastCodeTimestamp, starting };
     },
     computed: {
         elementId() {
@@ -48,9 +49,11 @@ export default {
 
         const resizeObserver = new ResizeObserver(entries => {
             clearTimeout(this.resizeTimeout);
-            this.resizeTimeout = setTimeout(async () => {
-                await this.init();
-            }, 500);
+            if(entries[0].contentRect.width && entries[0].contentRect.height){
+                this.resizeTimeout = setTimeout(async () => {
+                    await this.init();
+                }, 500);
+            }
         });
 
         resizeObserver.observe(this.$el);
@@ -67,20 +70,21 @@ export default {
                 } else {
                     newCamera = this.cameras.find(camera => camera.label === this.content.cameraName);
                 }
-                if (!newCamera) console.error(`Camera not found ${this.content.cameraName}`);
-                else {
+                if (newCamera) {
                     this.cameraId = newCamera.id;
-                    await this.stopScan();
-                    this.startScan();
+                    await this.init();
                 }
             }
         },
     },
     methods: {
         async init() {
+            if(this.starting) return;
+            this.starting = true;
+
             if (this.html5QrCode) {
                 await this.stopScan();
-                this.startScan();
+                await this.startScan();
             } else {
                 this.html5QrCode = new Html5Qrcode(this.elementId);
                 this.cameras = await Html5Qrcode.getCameras();
@@ -90,8 +94,9 @@ export default {
                     this.cameraId = this.cameras[0].id;
                 }
 
-                this.startScan();
+                await this.startScan();
             }
+            this.starting = false;
         },
         async stopScan() {
             const state = this.html5QrCode.getState();
@@ -100,7 +105,7 @@ export default {
             }
             await this.html5QrCode.clear();
         },
-        startScan() {
+        async startScan() {
             if (!this.html5QrCode || !this.cameraId) return;
 
             const rect = this.$el.getBoundingClientRect();
@@ -108,29 +113,34 @@ export default {
 
             const aspectRatio = rect.width / (rect.height || (rect.width * 16) / 9);
 
-            this.html5QrCode.start(
-                this.cameraId,
-                {
-                    aspectRatio: isNaN(aspectRatio) ? 9 / 16 : aspectRatio,
-                },
-                (decodedText, decodedResult) => {
-                    const code = decodedText;
-                    const format = decodedResult.result.format.formatName;
+            try {
+                await this.html5QrCode.start(
+                    this.cameraId,
+                    {
+                        aspectRatio: isNaN(aspectRatio) ? 9 / 16 : aspectRatio,
+                    },
+                    (decodedText, decodedResult) => {
+                        const code = decodedText;
+                        const format = decodedResult.result.format.formatName;
 
-                    if (format === 'QR_CODE') {
-                        const delay = Date.now() - this.lastCodeTimestamp;
-                        if (delay < 1000 && code === this.codeValue) this.lastCodeTimestamp = Date.now();
-                        else {
-                            this.lastCodeTimestamp = Date.now();
-                            this.setCodeValue(code);
-                            this.$emit('trigger-event', {
-                                name: 'scan',
-                                event: { code: this.codeValue },
-                            });
+                        if (format === 'QR_CODE') {
+                            const delay = Date.now() - this.lastCodeTimestamp;
+                            if (delay < 1000 && code === this.codeValue) this.lastCodeTimestamp = Date.now();
+                            else {
+                                this.lastCodeTimestamp = Date.now();
+                                this.setCodeValue(code);
+                                this.$emit('trigger-event', {
+                                    name: 'scan',
+                                    event: { code: this.codeValue },
+                                });
+                            }
                         }
                     }
-                }
-            );
+                );
+            } catch (error) {
+                console.error(error);
+            }
+
         },
     },
 };
